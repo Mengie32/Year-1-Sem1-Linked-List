528196c766af8c2d362fd0ff4acff352a313c1d1
Mengie32 <gianluca.galea23@gmail.com>
2022-12-26 17:00:45 +0100

    Modified newIndexedNode. Added nodeAtIndex, deleteNode, & deleteNode

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -1,24 +1,52 @@
 #include "SingleLinkedList.h"
 #include <stdlib.h>
 
+// returns a pointer to the node at the specified index
+node* nodeAtIndex(node* listHead, int index) {
+	int i;
+	node* output = listHead;
+	for (i = 0; i < index; i++) {
+		output = output->next;
+	}
+	return output;
+}
 
-
-// Inserts a node into the list at the specified position (zero-indexed)
-node* newIndexedNode(node* listHead, void* data, int index) {
+// Inserts a node into the list at the specified position (zero-indexed).
+// Returns pointer to new node
+node* newIndexedNode(node **listHead, void* data, int index) {
 	int i;
 	node *newNode = (node*)malloc(sizeof(node)); // allocate memory for node (pointers only)
-	node *prev = listHead, *next;
+	node *prev = *listHead, *next;
 
-	// loop to just before the specified index position, setting prev in the process
-	for (i = 0; i < index-1; i++) {
-		prev = prev->next;
+	// special case if new node is meant to be the first node
+	if (!index) {
+		newNode->next = *listHead;
+		*listHead = newNode;
 	}
-	next = prev->next; // record what will be the next node in the list for later
+	else { // should this be an <if...else>, or an <if{return}> ?
 
-	//set pointers of previous and new node
-	newNode->next = next;
-	prev->next = newNode;
+		prev = nodeAtIndex(listHead, index - 1); // record what will be the previous node in the list for later
+		next = prev->next; // record what will be the next node in the list for later
 
-	// point new node to data
-	newNode->data = data;
+		//set pointers of previous and new node
+		newNode->next = next;
+		prev->next = newNode;
+
+		// point new node to data
+		newNode->data = data;
+	}
+
+	return newNode;
+}
+
+// Delete node at the specified index (zero-indexed).
+// Also deletes the data directly attached to the node!
+// (if the data contains pointers, the data attached to these pointers will not be freed)
+void deleteNode(node* listHead, int index) {
+	node* toDelete = nodeAtIndex(listHead, index);
+
+	// free data memory
+	free(toDelete->data);
+	// free pointers
+	free(toDelete);
 }
--- SingleLinkedList.h
+++ SingleLinkedList.h
@@ -1,14 +1,27 @@
 #ifndef SINGLELINKEDLIST_H
 #define SINGLELINKEDLIST_H
 
-// node with pointers tod data and the next node. For single-linked Lists
+// node with pointers to data and the next node. For single-linked Lists
 typedef struct node {
 	void* data;
 	struct node* next;
 }node;
 
+// returns a pointer to the node at the specified index
+node* nodeAtIndex(node* listHead, int index);
+
 // Inserts a node into the list at the specified position (zero-indexed)
-node* newIndexedNode(node* listHead, void* data, int index);
+// returns pointer to new node
+node* newIndexedNode(node** listHead, void* data, int index);
+
+// Delete node at the specified index (zero-indexed).
+// Also deletes the data attached to the node!
+void deleteNode(node* listHead, int index);
+
+// Delete node at the specified index (zero-indexed).
+// Also deletes the data directly attached to the node!
+// (if the data contains pointers, the data attached to these pointers will not be freed)
+void deleteNode(node* listHead, int index);
 
 #endif // !SINGLELINKEDLIST_H
 
